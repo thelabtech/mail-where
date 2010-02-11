c82e95df278d0ddd221ada9840dc3ac0dc64f048
Josh Starcher <josh.starcher@gmail.com>
2010-02-11 17:14:27 -0500

    add proxy back in

--- app/models/google_groups_api.rb
+++ app/models/google_groups_api.rb
@@ -98,7 +98,7 @@ class GoogleGroupsApi
   
   protected 
     def self.post(url, data)
-      response = `curl -X POST #{curl_headers} #{url} #{data_clause(data)}`
+      response = `curl -X POST #{curl_headers} #{url} #{data_clause(data)} -x proxy.ccci.org:8080`
       Rails.logger.debug('=================================================')
       Rails.logger.debug(url)
       Rails.logger.debug(response)
@@ -108,7 +108,7 @@ class GoogleGroupsApi
     end
     
     def self.contact_post(url, data)
-      response = `curl -X POST #{curl_contact_headers} #{url} #{data_clause(data)}`
+      response = `curl -X POST #{curl_contact_headers} #{url} #{data_clause(data)} -x proxy.ccci.org:8080`
       Rails.logger.debug('=================================================')
       Rails.logger.debug(url)
       Rails.logger.debug(response)
